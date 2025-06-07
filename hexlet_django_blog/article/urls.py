ec631e070624252286d9f55883e97d84eb9cb60f
SergeyAnuf <sergeysyava38@gmail.com>
2025-06-07 14:03:12 +0300

    1

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (
+    IndexView, 
+    ArticleView, 
+    ArticleFormCreateView, 
+    ArticleFormEditView,
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name='articles_index'),
@@ -8,4 +13,6 @@ urlpatterns = [
     path("<int:id>/", ArticleView.as_view(), name='article_show'),
     # Маршрут для добавления статей
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
+    # Маршрут для обновления статьи
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
 ]
